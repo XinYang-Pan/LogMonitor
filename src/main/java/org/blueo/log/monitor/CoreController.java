baf8b53f658bf9fde00d236742804f35c0203624
Pan, Xinyang <seanpan2008@gmail.com>
2017-02-06 11:41:30 +0800

    Signed-off-by: Pan, Xinyang <seanpan2008@gmail.com>

--- src/main/java/org/blueo/log/monitor/CoreController.java
+++ src/main/java/org/blueo/log/monitor/CoreController.java
@@ -1,7 +1,6 @@
 package org.blueo.log.monitor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,6 @@ public class CoreController {
 	private CoreService coreService;
 
 	@RequestMapping("/content")
-	@SendTo("/content/123")
 	public String getContent() {
 		return Joiner.on("<br/>").join(coreService.getContent());
 	}
